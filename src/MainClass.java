f699304b5490ab4d3f557dfb3cb0a92f65c22f20
dagger <guptaharsh615@gmail.com>
2018-11-14 12:03:49 +0530

    Merge branch 'alarm' of https://github.com/Eunoia1729/DigitalClock into alarm
    # Conflicts:
    #	build/classes/MainClass$1.class
    #	build/classes/MainClass$2.class
    #	build/classes/MainClass$3.class
    #	build/classes/MainClass$4.class
    #	build/classes/MainClass$5.class
    #	build/classes/MainClass.class
    #	build/classes/MainClass.form
    #	build/classes/Time.class
    #	build/classes/alarmClock$1.class
    #	build/classes/alarmClock$2.class
    #	build/classes/alarmClock$3.class
    #	build/classes/alarmClock$4.class
    #	build/classes/alarmClock.class
    #	build/classes/alarmClock.form
    #	build/classes/daySong$1.class
    #	build/classes/daySong$2.class
    #	build/classes/daySong.class
    #	build/classes/daySong.form
    #	build/classes/stopwatch$3.class
    #	build/classes/stopwatch.class
    #	build/classes/stopwatch.form
    #	nbproject/private/private.properties
    #	nbproject/private/private.xml
    #	nbproject/project.properties
    #	src/Clock.java
    #	src/MainClass.form
    #	src/MainClass.java
    #	src/Time.java
    #	src/alarmClock.form
    #	src/alarmClock.java
    #	src/daySong.form
    #	src/daySong.java
    #	src/stopwatch.form

--- src/MainClass.java
+++ src/MainClass.java
@@ -58,6 +58,7 @@ public class MainClass extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton3 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         timeLabel = new javax.swing.JLabel();
         timeZoneCombo = new javax.swing.JComboBox<>();
@@ -66,17 +67,10 @@ public class MainClass extends javax.swing.JFrame {
         alarmBtn = new javax.swing.JButton();
         timerBtn = new javax.swing.JButton();
 
+        jButton3.setText("jButton3");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/settingsicon.png"))); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        timeLabel.setFont(new java.awt.Font("AnjaliOldLipi", 1, 48)); // NOI18N
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/settingsicon.png"))); // NOI18N
 
         timeLabel.setFont(new java.awt.Font("Cambria Math", 1, 48)); // NOI18N
@@ -108,6 +102,11 @@ public class MainClass extends javax.swing.JFrame {
 
         alarmBtn.setFont(new java.awt.Font("Ebrima", 1, 11)); // NOI18N
         alarmBtn.setText("ALARM");
+        alarmBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                alarmBtnActionPerformed(evt);
+            }
+        });
 
         timerBtn.setText("TIMER");
         timerBtn.setFont(new java.awt.Font("Ebrima", 1, 11));
@@ -187,7 +186,7 @@ public class MainClass extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
 
-        alarmC.setVisible(true);
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -198,6 +197,10 @@ public class MainClass extends javax.swing.JFrame {
     private void timerBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_timerBtnActionPerformed
         new GUITimer().setVisible(true);
     }//GEN-LAST:event_timerBtnActionPerformed
+
+    private void alarmBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_alarmBtnActionPerformed
+        alarmC.setVisible(true);
+    }//GEN-LAST:event_alarmBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -238,6 +241,7 @@ public class MainClass extends javax.swing.JFrame {
     private javax.swing.JLabel dateLabel;
     private javax.swing.JButton generateStopwatch;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel timeLabel;
     private javax.swing.JComboBox<String> timeZoneCombo;
     private javax.swing.JButton timerBtn;
