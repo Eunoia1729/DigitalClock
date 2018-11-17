52ca768108755d9a6a38498b81ea14d71239f4c8
eunoia_1729 <bkumarraju@gmail.com>
2018-11-18 03:34:46 +0530

    Chnaged ui of alarm

--- src/alarmClock.java
+++ src/alarmClock.java
@@ -1,4 +1,5 @@
 import jaco.mp3.player.MP3Player;
+import java.awt.Color;
 import java.io.File;
 import java.util.ArrayList;
 import javax.swing.*;
@@ -20,6 +21,7 @@ public class alarmClock extends javax.swing.JFrame {
         alarms = new ArrayList<Time>(1000);
         aSongs = new ArrayList<AlarmSongs>(1000);
         dSong = new daySong(aSongs);
+        getContentPane().setBackground(Color.black);
         setDefaultCloseOperation(this.DISPOSE_ON_CLOSE);
     }
     class timeVerifier extends InputVerifier {
@@ -83,11 +85,14 @@ public class alarmClock extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         label1.setAlignment(java.awt.Label.CENTER);
-        label1.setFont(new java.awt.Font("DejaVu Sans Mono", 0, 18)); // NOI18N
-        label1.setForeground(new java.awt.Color(255, 255, 255));
+        label1.setBackground(new java.awt.Color(0, 0, 0));
+        label1.setFont(new java.awt.Font("Century Schoolbook L", 1, 24)); // NOI18N
+        label1.setForeground(new java.awt.Color(0, 255, 255));
         label1.setText("ALARM MANAGER");
 
-        hour1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
+        hour1.setBackground(new java.awt.Color(0, 0, 0));
+        hour1.setFont(new java.awt.Font("Century Schoolbook L", 1, 36)); // NOI18N
+        hour1.setForeground(new java.awt.Color(0, 255, 255));
         hour1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         hour1.setText("00");
         hour1.setToolTipText("");
@@ -98,12 +103,16 @@ public class alarmClock extends javax.swing.JFrame {
             }
         });
 
-        minute1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
+        minute1.setBackground(new java.awt.Color(0, 0, 0));
+        minute1.setFont(new java.awt.Font("Century Schoolbook L", 1, 36)); // NOI18N
+        minute1.setForeground(new java.awt.Color(0, 255, 255));
         minute1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         minute1.setText("00");
         minute1.setInputVerifier(new timeVerifier());
 
-        second1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
+        second1.setBackground(new java.awt.Color(0, 0, 0));
+        second1.setFont(new java.awt.Font("Century Schoolbook L", 1, 36)); // NOI18N
+        second1.setForeground(new java.awt.Color(0, 255, 255));
         second1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         second1.setText("00");
         second1.setInputVerifier(new timeVerifier());
@@ -113,14 +122,22 @@ public class alarmClock extends javax.swing.JFrame {
             }
         });
 
+        addAlarmButton.setForeground(new java.awt.Color(0, 255, 255));
         addAlarmButton.setText("Add Alarm");
+        addAlarmButton.setOpaque(false);
+        addAlarmButton.setContentAreaFilled(false);
+        addAlarmButton.setBorderPainted(false);
         addAlarmButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addAlarmButtonActionPerformed(evt);
             }
         });
 
+        editAlarmButton.setForeground(new java.awt.Color(0, 255, 255));
         editAlarmButton.setText("Edit Alarms");
+        editAlarmButton.setOpaque(false);
+        editAlarmButton.setContentAreaFilled(false);
+        editAlarmButton.setBorderPainted(false);
         editAlarmButton.setToolTipText("");
         editAlarmButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -129,16 +146,19 @@ public class alarmClock extends javax.swing.JFrame {
         });
 
         label2.setAlignment(java.awt.Label.CENTER);
+        label2.setBackground(new java.awt.Color(0, 0, 0));
         label2.setFont(new java.awt.Font("FreeSerif", 1, 48)); // NOI18N
-        label2.setForeground(new java.awt.Color(255, 255, 255));
+        label2.setForeground(new java.awt.Color(0, 255, 255));
         label2.setText(":");
 
         label3.setAlignment(java.awt.Label.CENTER);
+        label3.setBackground(new java.awt.Color(0, 0, 0));
         label3.setFont(new java.awt.Font("FreeSerif", 1, 48)); // NOI18N
-        label3.setForeground(new java.awt.Color(255, 255, 255));
+        label3.setForeground(new java.awt.Color(0, 255, 255));
         label3.setText(":");
 
-        settingsButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/settingsicon.png"))); // NOI18N
+        settingsButton.setBackground(new java.awt.Color(0, 0, 0));
+        settingsButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/settingsicon.png")));
         settingsButton.setToolTipText("");
         settingsButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -146,6 +166,7 @@ public class alarmClock extends javax.swing.JFrame {
             }
         });
 
+        daySelect.setForeground(new java.awt.Color(0, 255, 255));
         daySelect.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", " " }));
         daySelect.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -158,9 +179,9 @@ public class alarmClock extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(121, 121, 121)
-                .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(58, 58, 58)
+                .addGap(68, 68, 68)
+                .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(settingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18))
             .addGroup(layout.createSequentialGroup()
@@ -182,7 +203,7 @@ public class alarmClock extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(label3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(second1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(second1, javax.swing.GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE)))
                 .addContainerGap(86, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -191,22 +212,21 @@ public class alarmClock extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(settingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(second1, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(22, 22, 22)
+                                .addComponent(hour1, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(14, 14, 14)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(label3, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(22, 22, 22)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(minute1, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(hour1, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(settingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(second1, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addComponent(minute1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(addAlarmButton)
@@ -221,9 +241,8 @@ public class alarmClock extends javax.swing.JFrame {
         hour1.getAccessibleContext().setAccessibleName("hoursField");
         minute1.getAccessibleContext().setAccessibleName("minutesField");
         second1.getAccessibleContext().setAccessibleName("secondsField");
-        addAlarmButton.getAccessibleContext().setAccessibleName("addAlarm");
-        editAlarmButton.getAccessibleContext().setAccessibleName("editAlarms");
-        settingsButton.getAccessibleContext().setAccessibleName("alarmSettings");
+        addAlarmButton.getAccessibleContext().setAccessibleName("");
+        editAlarmButton.getAccessibleContext().setAccessibleName("");
         daySelect.getAccessibleContext().setAccessibleName("weekDay");
 
         pack();
