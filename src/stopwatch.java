50956a929669157539dd57d763d46b5b00408226
dagger <guptaharsh615@gmail.com>
2018-11-20 12:59:02 +0530

    Merge branch 'master' of https://github.com/Eunoia1729/DigitalClock
    # Conflicts:
    #	build/classes/Time.class
    #	nbproject/private/private.properties
    #	nbproject/private/private.xml
    #	nbproject/project.properties

--- src/stopwatch.java
+++ src/stopwatch.java
@@ -8,7 +8,7 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
     int flag=1;
     int count=1;
     int hour=0,min=0,sec=0,miliSec=0;
-    String timeDisp="88:88:88",miliSecDisp="888";
+    String timeDisp="00:00:00",miliSecDisp="000";
     /**
      * Creates new form stopwatch
      */
@@ -19,14 +19,15 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         t=new Thread(this);
         reset();
     }
-    
+
     public void reset()
     {
         hour=0;min=0;sec=0;miliSec=0;
-        timeDisp="88:88:88";miliSecDisp="888";
+        timeDisp="00:00:00";miliSecDisp="000";
+        count=1;
         display();
     }
-    
+
     public void display()
     {
         timeShow.setText(timeDisp);
@@ -49,7 +50,7 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         else
             timeDisp=timeDisp+":"+Integer.toString(sec);
     }
-    
+
     public void setMiliSecCounter()
     {
         miliSecDisp="";
@@ -60,13 +61,13 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         else
             miliSecDisp=Integer.toString(miliSec);
     }
-    
+
     public void run()
     {
         try{
             while(true)
             {
-                miliSec++;
+                miliSec+=5;
                 if(miliSec>999)
                 {
                     sec++;
@@ -87,14 +88,14 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
                 setTimeCounter();
                 setMiliSecCounter();
                 display();
-                t.sleep(1);
+                t.sleep(4);
             }
         }
         catch(Exception ex)
         {
         }
     }
-    
+
     public void printTimeList()
     {
         String temp=Integer.toString(count)+". "+timeShow.getText();
@@ -121,21 +122,22 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        timeShow.setFont(new java.awt.Font("DigifaceWide", 3, 30)); // NOI18N
+        timeShow.setBackground(new java.awt.Color(255, 255, 255));
+        timeShow.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
         timeShow.setText("88:88:88");
 
-        miliSecondShow.setFont(new java.awt.Font("DigifaceWide", 3, 24)); // NOI18N
+        miliSecondShow.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
         miliSecondShow.setForeground(new java.awt.Color(0, 102, 102));
         miliSecondShow.setText("888");
 
-        startStop.setText("START");
+        startStop.setText("Start");
         startStop.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 startStopActionPerformed(evt);
             }
         });
 
-        reset.setText("RESET");
+        reset.setText("Reset");
         reset.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 resetActionPerformed(evt);
@@ -147,50 +149,50 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         timeList.setRows(5);
         jScrollPane1.setViewportView(timeList);
 
-        jLabel1.setFont(new java.awt.Font("Algerian", 3, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Georgia", 0, 24)); // NOI18N
         jLabel1.setText(" STOPWATCH");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(62, 62, 62)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(17, 17, 17)
-                        .addComponent(timeShow, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(miliSecondShow, javax.swing.GroupLayout.DEFAULT_SIZE, 83, Short.MAX_VALUE)
-                        .addGap(45, 45, 45))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jScrollPane1)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(startStop, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(reset, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                        .addGap(64, 64, 64))))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(102, 102, 102)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(62, 62, 62)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(17, 17, 17)
+                                                .addComponent(timeShow, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(miliSecondShow, javax.swing.GroupLayout.DEFAULT_SIZE, 83, Short.MAX_VALUE)
+                                                .addGap(45, 45, 45))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                                        .addComponent(jScrollPane1)
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addComponent(startStop, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addGap(18, 18, 18)
+                                                                .addComponent(reset, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                                .addGap(64, 64, 64))))
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(112, 112, 112)
+                                .addComponent(jLabel1)
+                                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(timeShow, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(miliSecondShow, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(startStop)
-                    .addComponent(reset))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
-                .addContainerGap())
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(timeShow, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(miliSecondShow, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(startStop)
+                                        .addComponent(reset))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
+                                .addContainerGap())
         );
 
         pack();
@@ -198,9 +200,9 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
 
     private void startStopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startStopActionPerformed
         // TODO add your handling code here:
-        if(startStop.getText().equals("START"))
+        if(startStop.getText().equals("Start"))
         {
-            startStop.setText("STOP");
+            startStop.setText("Stop");
             if(flag==1){
                 t.start();
                 flag=0;
@@ -210,7 +212,7 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         }
         else
         {
-            startStop.setText("START");
+            startStop.setText("Start");
             t.suspend();
             printTimeList();
         }
@@ -220,7 +222,7 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         // TODO add your handling code here:
         reset();
         t.suspend();
-        startStop.setText("START");
+        startStop.setText("Start");
         timeList.setText("");
     }//GEN-LAST:event_resetActionPerformed
 
@@ -231,7 +233,7 @@ public class stopwatch extends javax.swing.JFrame implements Runnable
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
